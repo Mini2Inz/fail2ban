21363f223a96f3e2b8ef2ab8f7c58ea4f24fe869
Radosław Dąbrowski <radek.dab@gmail.com>
2017-12-18 00:26:11 +0100

    Receive message from connected client

--- fail2ban/server/shareserver.py
+++ fail2ban/server/shareserver.py
@@ -2,7 +2,7 @@ __author__    = "Radosław Dąbrowski"
 __copyright__ = "Copyright (c) 2017 Radosław Dąbrowski"
 __license__   = "GPL"
 
-import asyncore, socket
+import socket, asyncore, asynchat
 from ..helpers import getLogger
 
 ADDR    = ''
@@ -14,6 +14,7 @@ logSys = getLogger(__name__)
 
 # Useful links:
 #   - asyncore.dispatcher: https://docs.python.org/2/library/asyncore.html#asyncore-example-basic-echo-server
+#   - asynchat.async_chat: https://docs.python.org/2/library/asynchat.html#module-asynchat
 
 
 class ShareServer(asyncore.dispatcher):
@@ -31,3 +32,24 @@ class ShareServer(asyncore.dispatcher):
         if client is not None:
             sock, (addr, port) = client
             logSys.debug("Incoming connection from %s:%d", addr, port)
+            handler = CommandHandler(sock, addr, port)
+
+
+class CommandHandler(asynchat.async_chat):
+
+    def __init__(self, sock, addr, port):
+        asynchat.async_chat.__init__(self, sock=sock)
+        self._addr = addr
+        self._port = port
+        self._buffer = []
+        self.set_terminator("\n")
+        logSys.debug("Created CommandHandler for %s:%d", addr, port)
+
+    def collect_incoming_data(self, data):
+        self._buffer.append(data)
+
+    def found_terminator(self):
+        msg = "".join(self._buffer)
+        self._buffer = []
+        logSys.debug("Received message from %s:%d: %s", \
+            self._addr, self._port, msg)
